d2bdcc0a20fb987da0df98c491ec6572e3635d68
Matthew <mattydiggory@gmail.com>
2021-06-06 18:35:43 +0100

    event time line & add event model

--- src/app/activity-events/activity-events.component.ts
+++ src/app/activity-events/activity-events.component.ts
@@ -1,8 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, provideRoutes } from '@angular/router';
 import { Activity } from '../activities/activity';
 import { ActivityService } from '../services/activity-service';
+import { Rating } from './rating';
 
 @Component({
   selector: 'app-activity-events',
@@ -12,6 +14,10 @@ import { ActivityService } from '../services/activity-service';
 export class ActivityEventsComponent implements OnInit {
 
   public activity: Activity = { id: -1, title: "", description: "", created: new Date, imageUrl: "", events: [] };
+  public selectedFile: any;
+  public currentDate: Date = new Date();
+  public id: number = -1;
+  public ratings: Rating[] = [{ id: 1, name: "POOR" }, { id: 2, name: "BELOW AVERAGE" }, { id: 3, name: "AVERAGE" }, { id: 4, name: "GOOD" }, { id: 5, name: "EXCELLENT" }];
 
   constructor(private activityService: ActivityService, private route: ActivatedRoute) { }
 
@@ -26,13 +32,59 @@ export class ActivityEventsComponent implements OnInit {
     )
   }
 
+  onFileChanged(event: any, activityId: number) {
+
+    this.selectedFile = event.target.files[0];
+    this.onUpload(activityId);
+  }
+
+  onUpload(activityId: number) {
+
+    const uploadData = new FormData();
+    uploadData.append('file', this.selectedFile);
+
+    this.activityService.uploadActivityImage(uploadData, activityId)
+      .subscribe((response) => {
+        console.log("file upload response", response);
+        this.ngOnInit();
+      },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+  }
+
+  updateActivity(activity: Activity): void {
+    this.activityService.updateActivity(activity).subscribe(
+      (response: Activity) => {
+        this.getActivity(activity.id);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+  addEvent(addEventForm: NgForm): void {
+    document.getElementById('add-event-close')?.click();
+    this.activityService.addActivity(addEventForm.value).subscribe(
+      (response: Activity) => {
+        this.getActivity(this.id);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+    addEventForm.reset();
+  }
+
   ngOnInit(): void {
 
     this.route.paramMap.subscribe(params => {
-      let id = params.get('id');
+      this.id = parseInt(params.get('id') || "-1");
     });
 
-    //this.getActivity(id);
+    this.getActivity(this.id);
   }
 
 }
